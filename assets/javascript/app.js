241047a5fbd2b571ca1715974101c368b3d4f114
Joseph Vega <1josephvega@gmail.com>
2018-03-22 22:21:11 -0400

    JQuery function t add a new user to the Firebase, test file used fr verification will be deleted after I do the same thing for the signIn function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,5 @@
 // FireBase script to add to html
-{/* <script src="https://www.gstatic.com/firebasejs/4.12.0/firebase.js"></script> */}
+{/* <script src="https://www.gstatic.com/firebasejs/4.12.0/firebase.js"></script> */ }
 
 // ===========================Initialize FireBase===========================
 var config = {
@@ -7,55 +7,82 @@ var config = {
     authDomain: "recipe-master-7f905.firebaseapp.com",
     databaseURL: "https://recipe-master-7f905.firebaseio.com",
     storageBucket: "recipe-master-7f905.appspot.com"
-  };
+};
 
-  firebase.initializeApp(config);
+firebase.initializeApp(config);
 
 var database = firebase.database();
 
+// Global variables
+var userName = "";
+var password = "";
+var email = "";
+
 // ===========================User Account Management===========================
 
-// NEED: Function to house account creation section 
+// NEED: Function to house account creation section
+$("#submit").on("click", function (event) {
+    event.preventDefault();
     // variables to capture user email and password inputs for use in the createUser method
+    userName = $("#name").val().trim();
+    password = $("#reenter-password").val().trim()
+    email = $("#email").val().trim();
+
+    console.log(userName);
+    console.log(password);
+    console.log(email);
+
+    // Check that password entries match and are correct length
+    if ($("#password").val().trim() != $("#reenter-password").val().trim()) {
+        alert('Passwords do not match.');
+        return;
+    }
 
     // createUser method to be linked to account creation form in html
-    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function (error) {
         // Handle Errors here.
         var errorCode = error.code;
         var errorMessage = error.message;
         // Add error if password doesn't meet our specifications
+        if (errorCode == 'auth/weak-password') {
+            alert('The password is too weak.');
+        } else {
+            alert(errorMessage);
+        }
+        console.log(error);
     });
-
+});
 // close account creation section===========================
 
 // NEED: Create onClick function to house account login section 
-    // variables to capture user email and password inputs for use in the signIn method
+// variables to capture user email and password inputs for use in the signIn method
 
-    // signIn method to be linked to account login form in html
-    firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // ...
-      });
+// signIn method to be linked to account login form in html
+firebase.auth().signInWithEmailAndPassword(email, password).catch(function (error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ...
+});
 
 // close account login section===========================
 
 // NEED: Function to setup UI event listeners and register Firebase auth listeners
 
-    // Listen for changes to auth state
-    firebase.auth().onAuthStateChanged(function(user) {
+// Listen for changes to auth state
+firebase.auth().onAuthStateChanged(function (user) {
 
-        if (user) {
-            // User is signed in
-            var email = user.email;
-            var uid = user.uid;
+    if (user) {
+        // User is signed in
+        var userName = user.userName;
+        var email = user.email;
+        var uid = user.uid;
 
-        } else {
-            // User is signed out
-        }
+    } else {
+        // User is signed out
+    }
 
-    });
+});
 
 // close auth state listener section===========================
 
